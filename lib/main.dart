012a73bd728dc12e1788881b5a55aea98f7a8b50
Dmitry Balahonov <balahonov.dmitry@gmail.com>
2020-12-24 15:16:32 +0300

    refactor: clear project.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:star_wars_game/UI/screen/statistic_page.dart';
-import 'package:star_wars_game/models/star_wars_game.dart';
-import 'UI/screen/game_page.dart';
-import 'UI/screen/home_page.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -13,25 +8,10 @@ void main() {
 class StarWarsRandom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<StarWarsGame>(
-      create: (context) => StarWarsGame(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        initialRoute: '/',
-        routes: {
-          '/': (context) => HomePage(),
-          '/game': (context) => GamePage(),
-          '/statistic' : (context) => StatisticPage(),
-        },
-        title: 'Star Wars Game',
-        theme: ThemeData(
-          primarySwatch: Colors.blueGrey,
-          backgroundColor: Colors.white,
-          textTheme: GoogleFonts.robotoSlabTextTheme(
-            Theme.of(context).textTheme,
-          ),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Star Wars Game',
+      theme: ThemeData(),
     );
   }
 }
